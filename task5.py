2ef2300bec6ee473291fd902397e086c612bba6c
savithrik199 <72515478+savithrik199@users.noreply.github.com>
2020-10-16 23:40:52 +0530

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,3 +1,5 @@
+task on conditional statement
+
 x=int(input("enter first number  "))
 y=int(input("enter second number  "))
 '''enter z as per
@@ -21,5 +23,25 @@ elif(z==4):
 else:
     print("invalid z")
 
+#output
+enter first number  4
+enter second number  5
+enter z  1
+addition of 4 and 5 is 9
+
+enter first number  7
+enter second number  9
+enter z  2
+subtraction of 7 and 9 is -2 
+
+enter first number  -3
+enter second number  -5
+enter z  3
+multiplicati of -3 and  -5 is 15
+
+enter first number  -72
+enter second number  9
+enter z  4
+division of -72 and 9 is -8.0
 
 
